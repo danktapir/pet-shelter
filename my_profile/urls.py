07ed46ab7c0de1934f09299e83e776a38a3548cd
Rasyad Jabbar Poncosewoyo <rasyadponcosewoyo@gmail.com>
2023-12-05 20:50:55 +0700

    implemented my_profile's functionality (finished)

--- my_profile/urls.py
+++ my_profile/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'my_profile'
 
 urlpatterns = [
-    path('my-profile/', views.index, name='index'),
+    path('my-profile/index/', views.index, name='index'),
     path('edit-profile/', views.edit_profile, name='edit_profile'),
 ]
--- my_profile/views.py
+++ my_profile/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .forms import EditProfileForm
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your views here.
 def index(request):
